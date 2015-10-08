18799febed0abb82a41df20b94adfa8514052ffd
Eliav Maman <eliav@myinpact.com>
2015-10-09 01:34:17 +0300

    takanon

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,38 +1,49 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-  function ($scope, $http, $location, Authentication) {
-    $scope.authentication = Authentication;
+angular.module('users').controller('AuthenticationController',
+  ['$scope', '$http', '$location', 'Authentication', '$modal',
+    function ($scope, $http, $location, Authentication, $modal) {
+      $scope.authentication = Authentication;
 
-    // If user is signed in then redirect back home
-    if ($scope.authentication.user) $location.path('/');
+      // If user is signed in then redirect back home
+      if ($scope.authentication.user) $location.path('/');
 
-    $scope.credentials = {
-      userType:'promoter'
-    };
+      $scope.credentials = {
+        userType: 'promoter'
+      };
 
-    $scope.signup = function () {
-      $http.post('/auth/signup', $scope.credentials).success(function (response) {
-        // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+      $scope.signup = function () {
+        $http.post('/auth/signup', $scope.credentials).success(function (response) {
+          // If successful we assign the response to the global user model
+          $scope.authentication.user = response;
 
-        // And redirect to the index page
-        $location.path('/');
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
+          // And redirect to the index page
+          $location.path('/');
+        }).error(function (response) {
+          $scope.error = response.message;
+        });
+      };
 
-    $scope.signin = function () {
-      $http.post('/auth/signin', $scope.credentials).success(function (response) {
-        // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+      $scope.showTakanon = function () {
+        var modalInstance = $modal.open({
+          animation: true,
+          templateUrl: 'modules/users/views/authentication/takanon.html',
+          //controller: 'ModalInstanceCtrl',
+          size: 'lg'
 
-        // And redirect to the index page
-        $location.path('/');
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
-  }
-]);
+        });
+      };
+
+      $scope.signin = function () {
+        $http.post('/auth/signin', $scope.credentials).success(function (response) {
+          // If successful we assign the response to the global user model
+          $scope.authentication.user = response;
+
+          // And redirect to the index page
+          $location.path('/');
+        }).error(function (response) {
+          $scope.error = response.message;
+        });
+      };
+    }
+  ]);
