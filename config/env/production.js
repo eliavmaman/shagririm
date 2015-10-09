71a4b1870e5e4747afcff2e47c9eb72269822c07
Eliav Maman <eliav@myinpact.com>
2015-10-09 17:52:37 +0300

    asdfggg

--- config/env/production.js
+++ config/env/production.js
@@ -2,30 +2,30 @@
 
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/shagririm',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-				'public/lib/bootstrap-rtl/dist/css/bootstrap-rtl.css'
-			],
-			js: [
-				'public/lib/jquery/dist/jquery.js',
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/ng-s3upload/build/ng-s3upload.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
-	},
+	//assets: {
+	//	lib: {
+	//		css: [
+	//			'public/lib/bootstrap/dist/css/bootstrap.min.css',
+	//			'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+	//			'public/lib/bootstrap-rtl/dist/css/bootstrap-rtl.css'
+	//		],
+	//		js: [
+	//			'public/lib/jquery/dist/jquery.js',
+	//			'public/lib/angular/angular.min.js',
+	//			'public/lib/angular-resource/angular-resource.js',
+	//			'public/lib/angular-cookies/angular-cookies.js',
+	//			'public/lib/angular-animate/angular-animate.js',
+	//			'public/lib/angular-touch/angular-touch.js',
+	//			'public/lib/angular-sanitize/angular-sanitize.js',
+	//			'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+	//			'public/lib/angular-ui-utils/ui-utils.min.js',
+	//			'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+	//			'public/lib/ng-s3upload/build/ng-s3upload.js'
+	//		]
+	//	},
+	//	css: 'public/dist/application.min.css',
+	//	js: 'public/dist/application.min.js'
+	//},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
 		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
